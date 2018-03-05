a0c919004b5d86822b552e1ecb866ab19a923ca2
farahmed@gmail.com <farahmed@gmail.com>
2018-03-05 12:59:27 -0600

    checkpoint-7-RTD-PropsandState:completed

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -3,14 +3,27 @@ import './App.css';
  import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+  todos: [
+    { description: 'Make dinner', isCompleted: true },
+    { description: 'Help kids with homework', isCompleted: false },
+    { description: 'Work on Bloc', isCompleted: false }
+  ]
+};
+ }
+
   render() {
     return (
       <div className="App">
       <ul>
-      <ToDo />
-      <ToDo />
-    </ul>             
-      </div>
+      { this.state.todos.map( (todo, index) =>
+        <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+      )}
+    </ul>
+  </div>
+
     );
   }
 }
